f8b00add3fd0320b419256498ebcd98ec4e5bfb5
OudomMunint <108245473+OudomMunint@users.noreply.github.com>
2022-11-24 15:10:16 +1100

    Test db edit

--- DB/TstDB.sql
+++ DB/TstDB.sql
@@ -925,4 +925,36 @@ FROM Reservation, ResourceLocation, AllResource
 WHERE ReName =  'Media Suite' AND CONVERT(DATE, ReservationDateTime) IN ('2020-05-01', '2020-06-05', '2020-09-19')
 Group by ReservationDateTime, ReName;
 
+--Dev: Pannha Oudom Munint, L2, Jr SWE
+--Push to QA and TST not PROD
+--From DEV
+--External query for Student DB
 
+Select * from Register
+Select * from Student
+Select * from Semester
+Select * from Course
+
+Select count(*) from Register
+where grade IN ('A','A+','B+') and courseID like '%2040'
+
+Select courseID, count(stdNo) from Register
+Group by courseID
+
+Select courseID, count(stdNo) from Register
+where grade is not NULL
+Group by courseID
+
+Select stdNo, count(distinct courseID) from Register
+group by stdNo
+
+Select courseID, count(stdNo) from Register
+--where grade is not NULL
+Group by courseID
+having count(stdNo) > 0
+order by count(stdNo) desc
+
+Select stdNo, avg(mark) as "Marks" from Register
+where grade is not NULL
+group by stdNo
+having count(stdNo)>1

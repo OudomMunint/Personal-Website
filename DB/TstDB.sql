b036462d886b68f4d34524c79c027352b82dd397
OudomMunint <108245473+OudomMunint@users.noreply.github.com>
2022-11-24 15:09:15 +1100

    tstDb edit

--- DB/TstDB.sql
+++ DB/TstDB.sql
@@ -356,3 +356,36 @@ from LOAN;
 Select amount from LOAN
 group by amount
 ORDER BY (amount) DESC;
+
+--Query for hotel DB
+--TST AND QA ONLY
+
+select * from hotel
+--where city = 'Newcastle'
+where city like '%New%'
+
+select g from guest
+where guestAdress like '%Street%'
+order by guestName
+
+select * from Room
+where (type like '%D' or type like'%F') and price >180
+order by price asc
+
+select * from booking
+where dateto is Null
+
+select count(*) as "Number Of Hotel" from hotel
+
+select avg(price) as "Average Price of rooms" from room
+
+select sum(price) as "Sum Price of rooms" from room
+where type like '%F'
+
+select guestNo, count(roomNo) From booking
+where roomno like '%1'
+group by guestNo
+
+Select hotelNo, count(roomNo) as "Number of rooms in hotel" from room
+group by hotelNo
+order by hotelNo

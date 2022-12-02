14095a8d91217e5fa90aba6fca108ba95afdf4e3
OudomMunint <108245473+OudomMunint@users.noreply.github.com>
2022-12-02 22:17:10 +1100

    Update TstDB.sql

--- DB/TstDB.sql
+++ DB/TstDB.sql
@@ -958,3 +958,31 @@ Select stdNo, avg(mark) as "Marks" from Register
 where grade is not NULL
 group by stdNo
 having count(stdNo)>1
+Select * from Register
+Select * from Student
+Select * from Semester
+Select * from Course
+
+Select count(*) from Register
+where grade IN ('A','A+','B+') and courseID like '%2040'
+
+Select courseID, count(stdNo) from Register
+Group by courseID
+
+Select courseID, count(stdNo) from Register
+where grade is not NULL
+Group by courseID
+
+Select stdNo, count(distinct courseID) from Register
+group by stdNo
+
+Select courseID, count(stdNo) from Register
+--where grade is not NULL
+Group by courseID
+having count(stdNo) > 0
+order by count(stdNo) desc
+
+Select stdNo, avg(mark) as "Marks" from Register
+where grade is not NULL
+group by stdNo
+having count(stdNo)>1

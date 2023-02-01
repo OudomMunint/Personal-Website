139ee1ad2a5d5635c02172cf5986e83f6cd3a0a5
OudomMunint <108245473+OudomMunint@users.noreply.github.com>
2023-02-01 23:24:24 +1100

    Clean up + form template

--- php/mail.php
+++ php/mail.php
@@ -12,4 +12,18 @@ mail($recipient, $message, $mailheader)
 or die("Error! your request could not be processed");
 
 echo"We will be in touch!";
+
+/* Template */
+/* $name = $_POST['firstname'];
+$email = $_POST['lastname'];
+$message = $_POST['email'];
+
+$mailheader = "From:".$firstname."<".$email.">\r\n";
+
+$recipient = "oudommunint@gmail.com";
+
+mail($recipient, $message, $mailheader)
+or die("Error! your request could not be processed");
+
+echo"We will be in touch!"; */
 ?>

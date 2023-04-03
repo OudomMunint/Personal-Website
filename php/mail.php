aed71f423b91079d5330c496b13244f008e39b5e
OudomMunint <108245473+OudomMunint@users.noreply.github.com>
2023-04-03 22:36:10 +1000

    PHP Test function

--- php/mail.php
+++ php/mail.php
@@ -26,4 +26,32 @@ mail($recipient, $message, $mailheader)
 or die("Error! your request could not be processed");
 
 echo"We will be in touch!"; */
+class ContactFormTest extends PHPUnit_Framework_TestCase
+{
+    public function testContactFormSubmission()
+    {
+        // Arrange
+        $postData = [
+            'firstname' => 'John',
+            'lastname' => 'Doe',
+            'email' => 'johndoe@example.com',
+            'message' => 'This is a test message'
+        ];
+
+        // Act
+        ob_start(); // Start output buffering
+        $_POST = $postData; // Set the POST data
+        include 'contact-form.php'; // Include the PHP code
+        $output = ob_get_clean(); // Get the output buffer
+
+        // Assert
+        $this->assertContains('We will be in touch!', $output);
+        // Check that the output contains the success message
+
+        // Assert that an email was sent
+        $this->assertTrue(mail_sent());
+        // Assumes you have a function called mail_sent() that checks if an email was sent
+    }
+}
+
 ?>

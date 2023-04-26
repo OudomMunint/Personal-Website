fce23c276de2b6b6bd2538aed5cbcb3d79c547ed
OudomMunint <oudommunint@gmail.com>
2023-04-26 23:30:34 +1000

    DB update

--- DB/ProjDB.sql
+++ DB/ProjDB.sql
@@ -1,6 +1,22 @@
-CREATE TABLE projects (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  heading VARCHAR(255) NOT NULL,
+CREATE TABLE Projects (
+  project_id INT PRIMARY KEY,
+  user_id INT NOT NULL,
+  title VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
-  image_url VARCHAR(255) NOT NULL
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  github_link VARCHAR(255) NOT NULL,
+  demo_link VARCHAR(255),
+  created_at DATETIME NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Users (
+  user_id INT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  created_at DATETIME NOT NULL
 );
